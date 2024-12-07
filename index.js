4927f5728c9ccf6fd49db986aa15c6d7121d4584
Cheriroh <prudencec2019@gmail.com>
2024-12-07 21:43:59 +0300

    phase-1-review-strings-lab

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-// Write your code in this file!
+// Define the currentUser variable as a string
+const currentUser = "Grace Hopper";
+
+// Define the welcomeMessage variable
+const welcomeMessage = `Welcome to Flatbook, ${currentUser}!`;
+
+// Define the excitedWelcomeMessage variable
+const excitedWelcomeMessage = `WELCOME TO FLATBOOK, ${currentUser.toUpperCase()}!`;
+
+// Define the shortGreeting variable
+const shortGreeting = `Welcome, ${currentUser[0]}!`;
